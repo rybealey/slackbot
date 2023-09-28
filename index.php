52f462814e0cfeb29faa0f37498dd24b0034a23e
Ry Bealey <hello@ryanbealey.com>
2023-09-27 21:52:59 -0500

    Slackbot configuration complete, creating routes and endpoints for command-based solutions.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE HTML>
+<html lang="en-us">
+    <head>
+        <title>Slackbot v0.0.1</title>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Slackbot v0.0.1 is running!</h1>
+        <p>The Slackbot is currently taking requests.</p>
+        <p>Token: <?php echo $_ENV("TOKEN"); ?></p>
+    </body>
+</html>
