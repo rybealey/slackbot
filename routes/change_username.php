30234b557cf287109fae2f4761583420bf6410e0
Ry Bealey <hello@ryanbealey.com>
2023-09-27 22:53:41 -0500

    Changing how botman is called.

--- routes/change_username.php
+++ routes/change_username.php
@@ -1,7 +1,20 @@
 <?php
 
     require __DIR__ . "/vendor/autoload.php";
-    require __DIR__ . "/slack.php";
+    
+    use BotMan\BotMan\BotMan;
+    use BotMan\BotMan\BotManFactory;
+    use BotMan\BotMan\Drivers\DriverManager;
+    DriverManager::loadDriver(\BotMan\Drivers\Slack\SlackDriver::class);
+
+    // Create the Slackbot instance.
+    $config = [
+        "slack" => [
+                "token" => "xoxb-526890004994-5786369407413-F63nkka4f06GYAjJH1eU4nkj"
+            ]
+        ];
+
+    $botman = BotManFactory::create($config);
 
     $botman->hear("!chusername {dsid} {current} {new} {reason}", function($bot, $dsid, $current, $new, $reason){
         $bot->reply("I have logged $dsid's username change from $current to $new for the reason of $reason and have timestamped the interaction in the database.");
